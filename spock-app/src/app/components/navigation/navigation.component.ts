b591e5318f4fceef5e3b891a3923248de616c1e9
vikaspachori <vikas_pachori@yahoo.com>
2021-07-28 02:01:13 +0530

    front end dev done

--- spock-app/src/app/components/navigation/navigation.component.ts
+++ spock-app/src/app/components/navigation/navigation.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navigation',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavigationComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+    if (window.location.href.includes("profile")) {
+      document.getElementById("profile").classList.add("nav-selected");
+    }
+    else {
+      document.getElementById("home").classList.add("nav-selected")
+    }
   }
-
+  itemClick(evt, url) {
+    const elm = (evt.target as Element);
+    const selectedClass = document.getElementsByClassName("nav-selected");
+    for (let index = 0; index < selectedClass.length; index++) {
+      const element = selectedClass[index];
+      element.classList.remove("nav-selected")
+    }
+    elm.classList.add("nav-selected")
+    this.router.navigateByUrl(url)
+  }
 }
