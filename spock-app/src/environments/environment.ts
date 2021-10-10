2670df973aa2d7455888f7e3d26acb57527652ca
vikaspachori <vikas_pachori@yahoo.com>
2021-10-10 22:11:50 +0530

    fixed https

--- spock-app/src/environments/environment.ts
+++ spock-app/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: "http://ec2-13-234-75-18.ap-south-1.compute.amazonaws.com:9090/api/Spock/v1/",
+  apiUrl: "https://spockapi.ewns.in/api/Spock/v1/",
   imageUrl: 'https://images.cricket.com/players/{0}_headshot_safari.png'
 };
 
